58eaa31500966ae4052cc5ca1160e78d40656c51
Matheus Venceslau <matheushlvenceslau@gmail.com>
2025-03-29 12:05:38 -0300

    Teste

--- src/models/Professor.java
+++ src/models/Professor.java
@@ -1,4 +1,35 @@
 package models;
 
 public class Professor {
+    private String nomeProfessor;
+    private String diciplinaProfessor;
+    private int idadeProfessor;
+
+    public Professor(String nomeProfessor, String diciplinaProfessor, int idadeProfessor) {
+        this.nomeProfessor = nomeProfessor;
+        this.diciplinaProfessor = diciplinaProfessor;
+        this.idadeProfessor = idadeProfessor;
+    }
+
+    public String getNomeProfessor() {
+        return nomeProfessor;
+    }
+
+    public String getDiciplinaProfessor() {
+        return diciplinaProfessor;
+    }
+
+    public int getIdadeProfessor() {
+        return idadeProfessor;
+    }
+    
+    @Override
+    public String toString() {
+        return "Professor{" +
+                "nomeProfessor='" + nomeProfessor + '\'' +
+                ", diciplinaProfessor='" + diciplinaProfessor + '\'' +
+                ", idadeProfessor=" + idadeProfessor +
+                '}';
+    }
+
 }
